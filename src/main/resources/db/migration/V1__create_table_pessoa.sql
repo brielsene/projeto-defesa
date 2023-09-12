7b081f9fb445adedb3cdee6cc61139c71943c65f
Gabriel Sene <sene300@gmail.com>
2023-09-11 23:31:02 -0300

    feat: Adicionando migration de criação dee tabela

---
+++ src/main/resources/db/migration/V1__create_table_pessoa.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PESSOAS(
+    id serial primary key,
+    nome varchar,
+    email varchar,
+    data_nascimento varchar,
+    private varchar cpf
+
+);
---
+++ src/test/java/br/com/dfense/rhservice/RhServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dfense.rhservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
